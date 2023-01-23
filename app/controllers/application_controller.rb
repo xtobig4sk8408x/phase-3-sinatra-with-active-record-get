d89b1727dbe32e02ac9f3e4ce4c7cefda045f795
Theodore John Josef <john.josef@aol.com>
2023-01-23 01:30:51 -0800

    commit tree 11dfe64ad446c3ed99a606f1e8185f09bea9a28a

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,24 @@ class ApplicationController < Sinatra::Base
     { message: "Hello world" }.to_json
   end
 
+  set :default_content_type, 'application/json'
+
+  get '/games' do 
+    #get all the games from the database
+    games = Game.all.order(:title).limit(10)
+    #return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  #use the :id syntax to create a dynamic route
+  get '/games/:id' do 
+    #look up the game in the database using it's ID
+    game = Game.find(params[:id])
+    #send a JSON-formatted response of the game data
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: {only: [:name] }
+      } }
+    })
+  end
 end
